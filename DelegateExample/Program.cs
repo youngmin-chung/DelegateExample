6485f27413c211c17dd4d64e22b60b7fa6e74668
Youngmin Chung <mintmin01@gmail.com>
2021-06-08 14:33:15 -0400

    Add project files.

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/*
+
+Terminology: Delegate
+* An objectified function
+  - Inherits from System.Delegate
+  - Sealed implicity
+  - Looks much like C/C++ style function pointer
+
+* A delegate maintains three important pieces of information
+  - The address of the method on which it makes calls
+  - The parameters (if any) of this method
+  - The return type (if any) of this method
+ 
+Type of Delegates
+* Singlecast Delegates: Points to a single method at a time.
+* Multicast Delegates: Points to more than one function at a time
+
+Important Points About Delegates:
+
+* Provides a good way to encapsulate the methods.
+* Delegates are the library class in System namespace.
+* These are the type-safe pointer of any method.
+* Delegates are mainly used in implementing the call-back methods and events.
+* Delegates can be chained together as two or more methods can be called on a single event.
+* It doesn’t care about the class of the object that it references.
+* Delegates can also be used in “anonymous methods” invocation.
+* Anonymous Methods(C# 2.0) and Lambda expressions(C# 3.0) are compiled to delegate types in certain contexts. 
+  Sometimes, these features together are known as anonymous functions.
+  
+ */
+
+namespace DelegateExample
+{
+    public class Program
+    {
+        public class MyDelegate
+        {
+            public delegate void DelegateExample(int a);
+
+            public static void PrintInt(int a)
+            {
+                Console.WriteLine("Parameter is "+a);
+            }
+
+            public static void PrintType<T>(T a)
+            {
+                Console.WriteLine("Type of parameter is "+a.GetType());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate example method d1() is ");
+            MyDelegate.DelegateExample d1 = MyDelegate.PrintInt;
+            d1(1);
+            Console.WriteLine("\nDelegate example method d2() is ");
+            MyDelegate.DelegateExample d2 = MyDelegate.PrintType;
+            d2(1);
+            Console.WriteLine("\nDelegate example method d3() is ");
+            MyDelegate.DelegateExample d3 = d1 + d2;
+            d3(1);
+            Console.WriteLine("\nDelegate example method d4() is ");
+            MyDelegate.DelegateExample d4 = d3 - d2;
+            d4(1);
+            Console.WriteLine("\nResult of d4() == d1() is ");
+            Console.WriteLine(d4 == d1);
+   
+        }
+    }
+}
---
+++ InClassActivity/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace InClassActivity
+{
+    public delegate int NumberChanger(int a);
+    public class Program
+    {
+        private static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultiNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        //The power of num
+        public static int PowerNum(int q)
+        {
+            num = (int)Math.Pow(num, q);
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            //Create delegate instances using the customized delegate
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultiNum);
+            NumberChanger nc3 = new NumberChanger(PowerNum);
+            //Calling the methods using the delegate objects
+            Console.WriteLine("A customized Delegate for each method");
+            Console.WriteLine("------------------------\n");
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", num);
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", num);
+            nc3(2);
+            Console.WriteLine("Value of Num: {0}", num);
+
+            //Reset the value of num
+            num = 10;
+            //The built-in Func delegate
+            Console.WriteLine("A built-in Delegate for each method");
+            Console.WriteLine("------------------------\n");
+            Func<int, int> add = AddNum;
+            Console.WriteLine("Call the adding method = " + add(2));
+
+            Func<int, int> Mult = MultiNum;
+            Console.WriteLine("Call the adding method = " + Mult(2));
+
+            Func<int, int> Power = PowerNum;
+            Console.WriteLine("Call the adding method = " + Power(2) + "\n");
+
+            //Reset the value of num
+            num = 10;
+            //Chain all methods with using only one delegate
+            Console.WriteLine("Only one built-in Delegate for all methods");
+            Console.WriteLine("------------------------\n");
+            Func<int, int> chainedDelegate = AddNum;
+            chainedDelegate += MultiNum;
+            chainedDelegate += PowerNum;
+
+            Console.WriteLine("The value of num after running the chained delegate = " + chainedDelegate(2));
+
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
